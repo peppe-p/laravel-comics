cfe2e80c352f503314626423824501ba60e9fcff
peppe-p <ombrachiara96@gmail.com>
2021-07-10 13:32:35 +0200

    add html and css for top blue bar on header

--- resources/views/structure.blade.php
+++ resources/views/structure.blade.php
@@ -4,13 +4,49 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>@yield('title')</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
 </head>
+
+<header>
+    <div class="blue_row">
+        <div class="container">
+            <span>dc power&#8480; visa&#174;</span>
+            <span>additional dc sites <i class="fas fa-caret-down"></i></span>
+        </div>
+    </div>
+
+    <div class="main_nav">
+        <div class="container">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="Logo DC Comics">
+            <ul>
+                <li><a href="#">Character</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
+            </ul>
+            <div class="search_box">
+                <input type="text" placeholder="Search">
+                <i class="fas fa-search"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="jumbo">
+        <div class="container">
+
+        </div>
+    </div>
+</header>
 
 <body>
     <section>
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $comics_data = config('comics');
     return view('home');
-});
+})->name('home');
